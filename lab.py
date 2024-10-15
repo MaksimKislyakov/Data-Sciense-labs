1c7fdf75e761909271824caafee3902bbfa6f229
MaksimKislyakov <kislakov.maksim.vyacheslavovich@gmail.com>
2024-10-15 19:45:56 +0500

    numpy

--- lab.py
+++ lab.py
@@ -1,37 +1,18 @@
-import unittest
-def num_balance(n: int) -> str:
-    half_length = 0
-    if n > 0: 
-        if len(str(n)) % 2 == 0:
-            half_length = int(len(str(n)) / 2)
-            sum_first_path = sum(int(digit) for digit in str(n)[:half_length])
-            sum_second_path = sum(int(digit) for digit in str(n)[half_length:])
-            if sum_first_path == sum_second_path:
-                return 'число сбалансировано'
-            return 'число не сбалансировано'
-        else:
-            half_length = int((len(str(n))) / 2)
-            sum_first_path = sum(int(digit) for digit in str(n)[:half_length+1])
-            sum_second_path = sum(int(digit) for digit in str(n)[half_length:])
-            if sum_first_path == sum_second_path:
-                return 'число сбалансировано'
-            return 'число не сбалансировано'
+import numpy as np
+
+def one_hot_encoding(labels):
+    # Находим количество уникальных классов (максимальное значение + 1)
+    num_classes = np.max(labels) + 1
     
-
-print(num_balance(12341234))
-print(num_balance(3335333))
-print(num_balance(4756447564))
-print(num_balance(123456))
-
-class Test(unittest.TestCase):
-    def test_num_balance(self):
-        self.assertEqual(num_balance(12341234), 'число сбалансировано')
-        self.assertEqual(num_balance(33533), 'число сбалансировано')
-        self.assertEqual(num_balance(4756447564), 'число сбалансировано')
+    # Создаем двумерный массив с нулями и заполняем его one-hot представлением
+    one_hot = np.zeros((len(labels), num_classes), dtype=int)
     
-    def test_num_not_balance(self):
-        self.assertEqual(num_balance(123456), 'число не сбалансировано')
-        self.assertEqual(num_balance(1231232), 'число не сбалансировано')
+    # Проставляем 1 в соответствующих позициях
+    for idx, label in enumerate(labels):
+        one_hot[idx, label] = 1
+    
+    return one_hot
 
-if __name__ == '__main__':
-    unittest.main()
+labels = [0, 2, 3, 0]
+one_hot_encoded = one_hot_encoding(labels)
+print("One-hot encoding:\n", one_hot_encoded)
